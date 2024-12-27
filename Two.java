f14b537ebe83bbdf0017f016909cde4362d4b0e1
sumodha <pokhrel.sumodha@gmail.com>
2024-12-26 20:15:06 -0600

    new class called Two

---
+++ Two.java
@@ -0,0 +1,7 @@
+public class Two{
+    public static void main(String[] args){
+        System.out.println("I love the number twooooo");
+
+        System.out.println("version control is very interesting");
+    }
+}
